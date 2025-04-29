95d8d7563141d0989d7396feb66050d119c8054b
Josue Paucar <josue_dpg@hotmail.com>
2025-04-28 21:11:56 -0500

    Modify DTO of request

--- API/WebApplication1/DTOs/Request/Purchase.cs
+++ API/WebApplication1/DTOs/Request/Purchase.cs
@@ -4,7 +4,5 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int? CatalogId { get; set; }
-        public string CatalogDetail { get; set; }
     }
 }
